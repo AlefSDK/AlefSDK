3c6ae3d0f2a8f536b68ce4edcf1cbe63573f3dcf
Lam Ngo <lam.ngo@alefedge.com>
2019-10-23 23:11:35 -0400

    Initial Commit

---
+++ AlefSDK/AlefSDK.h
@@ -0,0 +1,19 @@
+//
+//  AlefSDK.h
+//  AlefSDK
+//
+//  Created by Lam Ngo (Work) on 10/23/19.
+//  Copyright © 2019 AlefEdge, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlefSDK.
+FOUNDATION_EXPORT double AlefSDKVersionNumber;
+
+//! Project version string for AlefSDK.
+FOUNDATION_EXPORT const unsigned char AlefSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlefSDK/PublicHeader.h>
+
+
